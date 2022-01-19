ba2b8b36a0907a67424fb5d66ef1553a6e2c15a4
sangeunyoon <yse1468@gmail.com>
2022-01-19 19:09:49 +0000

    modified session2,3

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,7 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+const Greeting ='Hello everybody!'
+console.log(Greeting);  
+
+let Name='sangeun'
+console.log(Name);  
--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,10 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+var thisIsNumber= 10;//
+
+var THIS_IS_A_NUMBER = 10;
+console.log(thisIsNumber)
+
+const name ='Tanya';
+var age='10';
+console.log(name)

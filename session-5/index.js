0aa00f4429341fccba488ba635930e80365cdaac
sangeunyoon <yse1468@gmail.com>
2022-01-26 19:39:18 +0000

    added form memo box

---
+++ session-5/index.js
@@ -0,0 +1,44 @@
+function getGrade(total){
+  let grade;
+  switch (total){
+    case (total >90:)
+      grade = "A+";
+      break;
+      case 80;
+      grade= "A";
+      break;
+      default:
+        grade="Fail";
+  }
+}
+
+
+
+// function getGrade(total) {
+//   if (total > 90) {
+//     return "A+";
+//   } else if (total > 80 && total <= 90) {
+//     return "A";
+//   }else if (total > 70 && total <= 80) {
+//     return "B";
+// }else if (total > 60 && total <= 70) {
+//     return "C";
+// }else if (total > 50 && total <= 60) {
+//     return "D";
+// }else if (total > 40 && total <= 50) {
+//     return "E";
+// }else if (total > 30 && total <= 40) {
+//     return "F";
+// }else {return "fail"}
+// }
+
+// console.log(getGrade(40));
+
+// A+ if total is greater than 90.
+// ○ A if total is greater than 80 and less than or equal to 90.
+// ○ B if total is greater than 70 and less than or equal to 80.
+// ○ C if total is greater than 60 and less than or equal to 70.
+// ○ D if total is greater than 50 and less than or equal to 60.
+// ○ E if total is greater than 40 and less than or equal to 50.
+// ○ F if total is greater than 30 and less than or equal to 40.
+// ○ “Fail” is total is less than 30-
